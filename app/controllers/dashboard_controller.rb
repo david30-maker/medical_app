5f0838634c23ec6b28e1763ec50d726f0e9aace4
david30-maker <davidokpah396@gmail.com>
2024-12-10 00:09:24 +0100

    authorize doctor

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,2 +1,16 @@
 class DashboardController < ApplicationController
+    before_action :authenticate_user!
+    before_action :authorize_doctor
+
+    def graph
+        @data = Patient.group_by_day(:registered_at).count
+    end
+
+    private
+
+    def authorize_doctor
+        if !current_user.doctor?
+            redirect_to root_path, alert: "You are not authorized to access this page"
+        end
+    end
 end
